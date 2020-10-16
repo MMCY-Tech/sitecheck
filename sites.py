eeca8fc4eeb156ec5d6ab23df53737a5825299e5
beruhy <beruhy@gmail.com>
2020-10-16 19:40:21 +0300

    Update sites.py

--- sites.py
+++ sites.py
@@ -104,7 +104,7 @@ def check_sites(verbose=False):
                                   s['contents']))
 
     ssl_hosts = [
-        'mmcytech.com', 'app.mymeetingtracker.com', 'moderneth.com'
+        'mmcytech.com', 'app.mymeetingtracker.com'
     ]
     for s in ssl_hosts:
         if verbose:
